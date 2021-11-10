358347126c92ef4fa2567c519eb29bbcf5a0dd5f
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-10 16:10:15 +0000

    fix type errors and enable deno

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/fp-attempt-9001/mod.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/20d76e83c7c95123020a04f86ccef6b81732cb81/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,70 +1,76 @@
-import type { Bot } from "./deps.ts";
+import { Bot } from "./deps.ts";
 
 // PLUGINS MUST TAKE A BOT ARGUMENT WHICH WILL BE MODIFIED
-export function enableSyncCachePlugin(bot: Bot) {
+export function enableCachePlugin(bot: Bot): Bot {
   // MARK THIS PLUGIN BEING USED
-  if (bot.enabledPlugins) bot.enabledPlugins.add("CACHE");
-  else bot.enabledPlugins = new Set(["CACHE"]);
+  bot.enabledPlugins.add("CACHE");
 
   // CUSTOMIZATION GOES HERE
 
-  const caches = [
-    "guilds",
-    "users",
-    "members",
-    "channels",
-    "messages",
-    "presences",
-  ];
+  // Get the unmodified transformer.
+  const { guild, user, member, channel, message, presence } = bot.transformers;
+  // Override the transformer
+  bot.transformers.guild = function (...args) {
+    // Run the unmodified transformer
+    const result = guild(...args);
+    // Cache the result
+    bot.cache.guilds.set(result.id, result);
+    // Return the result
+    return result;
+  };
 
-  for (const key in caches) {
-    // Get the unmodified transformer.
-    const oldTransformer = bot.transformers[key];
-    // Override the transformer
-    bot.transformers[key] = function (...args) {
-      // Run the unmodified transformer
-      const result = oldTransformer(...args);
-      // Cache the result
-      bot.cache[key].set(result.id, result);
-      // Return the result
-      return result;
-    };
-  }
+  // Override the transformer
+  bot.transformers.user = function (...args) {
+    // Run the unmodified transformer
+    const result = user(...args);
+    // Cache the result
+    bot.cache.users.set(result.id, result);
+    // Return the result
+    return result;
+  };
 
-  // PLUGINS MUST RETURN THE BOT
-  return bot;
-}
+  // Override the transformer
+  bot.transformers.member = function (...args) {
+    // Run the unmodified transformer
+    const result = member(...args);
+    // Cache the result
+    bot.cache.members.set(
+      bot.transformers.snowflake(`${result.id}${result.guildId}`),
+      result,
+    );
+    // Return the result
+    return result;
+  };
 
-// PLUGINS MUST TAKE A BOT ARGUMENT WHICH WILL BE MODIFIED
-export async function enableAsyncCachePlugin(bot: Bot) {
-  // MARK THIS PLUGIN BEING USED
-  if (bot.enabledPlugins) bot.enabledPlugins.add("CACHE");
-  else bot.enabledPlugins = new Set(["CACHE"]);
+  // Override the transformer
+  bot.transformers.channel = function (...args) {
+    // Run the unmodified transformer
+    const result = channel(...args);
+    // Cache the result
+    bot.cache.channels.set(result.id, result);
+    // Return the result
+    return result;
+  };
 
-  // CUSTOMIZATION GOES HERE
+  // Override the transformer
+  bot.transformers.message = function (...args) {
+    // Run the unmodified transformer
+    const result = message(...args);
+    // Cache the result
+    bot.cache.messages.set(result.id, result);
+    // Return the result
+    return result;
+  };
 
-  const caches = [
-    "guilds",
-    "users",
-    "members",
-    "channels",
-    "messages",
-    "presences",
-  ];
-
-  for (const key in caches) {
-    // Get the unmodified transformer.
-    const oldTransformer = bot.transformers[key];
-    // Override the transformer
-    bot.transformers[key] = function (...args) {
-      // Run the unmodified transformer
-      const result = oldTransformer(...args);
-      // Cache the result
-      await bot.cache[key].set(result.id, result);
-      // Return the result
-      return result;
-    };
-  }
+  // Override the transformer
+  bot.transformers.presence = function (...args) {
+    // Run the unmodified transformer
+    const result = presence(...args);
+    // Cache the result
+    bot.cache.presences.set(result.user.id, result);
+    // Return the result
+    return result;
+  };
 
   // PLUGINS MUST RETURN THE BOT
   return bot;
